d2d1422664e8f785564f6f7e9711c6243d44e9da
Gerardo Marquez <gerardo.marquez@resideo.com>
2024-02-28 14:30:53 -0600

    update

--- T6ProInstallationInstructions/Data/Search.js
+++ T6ProInstallationInstructions/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Installation-Topics/1. UWP Mounting System.htm'],s:['+'],p:[['+',3],['section',1]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Installation-Topics/1. UWP Mounting System.htm'],s:['+'],p:[['+',3],['securely',7]]});
--- T6ProInstallationInstructions/Resources/Scripts/require.config.js
+++ T6ProInstallationInstructions/Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638447267267938859'
+    urlArgs: 't=638447273977154689'
 });
